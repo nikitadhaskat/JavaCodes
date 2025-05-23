651c61a298d44f2d5171393fc926a001d1625f4d
Nikita Dhaskat <nikitadhaskat2002@gmail.com>
2025-05-23 13:13:23 +0530

    Add files via upload

---
+++ checkAlphabet.java
@@ -0,0 +1,14 @@
+package practiceCodes;
+
+public class checkAlphabet {
+
+	public static void main(String[] args) 
+	{
+       char ch='7';
+       if(ch>=65 && ch<=90 ||ch>=97 && ch<=122)
+    	   System.out.println("It is a Alphabet");
+       else
+    	   System.out.println("It is not  Alphabet");
+	}
+
+}
---
+++ divisibleBy5or11.java
@@ -0,0 +1,19 @@
+package practiceCodes;
+
+public class divisibleBy5or11 {
+
+	public static void main(String[] args)
+	{
+       int n=56;
+       if(n%5==0 && n%11==0)
+       {
+    	   System.out.println("Number is divisible by 5 and 11");
+       }
+       else
+       {
+    	   System.out.println("Number is not divisible by 5 and 11");
+
+       }
+	}
+
+}
