32890ec380fe01b1adb462fbf86726b5ec5d2fb7
Nikita Dhaskat <nikitadhaskat2002@gmail.com>
2025-05-23 13:15:13 +0530

    Add files via upload

---
+++ checkPositiveNegative.java
@@ -0,0 +1,18 @@
+package practiceCodes;
+
+public class checkPositiveNegative {
+
+	public static void main(String[] args)
+	{
+       int num=0;
+       if(num<0)
+    	   System.out.println("Number is Negative");
+       else if(num>0)
+    	   System.out.println("Number is Positive");
+       else
+    	   System.out.println("Number is Zero");
+
+
+	}
+
+}
