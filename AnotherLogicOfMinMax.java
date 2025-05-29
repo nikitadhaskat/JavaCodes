ca0d6be0cf28f2689d893a0691467c51ed33a09d
Nikita Dhaskat <nikitadhaskat2002@gmail.com>
2025-05-29 22:20:25 +0530

    Add files via upload

---
+++ AnotherLogicOfMinMax.java
@@ -0,0 +1,21 @@
+
+public class AnotherLogicOfMinMax {
+
+	public static void main(String[] args)
+	{
+       int a=3,b=6,c=54;
+       if(a>b)
+           System.out.println("a is maximum");
+       else if(b>c)
+    	   System.out.println("b is maximum");
+       else
+    	   System.out.println("c is maximum");
+       if(a<b)
+    	   System.out.println("a is minimum");
+       else if(b<c)
+    	   System.out.println("a is minimum");
+       else 
+    	   System.out.println("c is minimum");
+	} 
+
+}
---
+++ ArithmaticandRelationalOperator.java
@@ -0,0 +1,23 @@
+
+public class ArithmaticandRelationalOperator {
+
+	public static void main(String[] args)
+	{
+       int a=25;
+       int b=25;
+       System.out.println(a+b);//Arithmatic operator
+       System.out.println(a-b);
+       System.out.println(a*b);
+       System.out.println(a/b);
+       System.out.println(a%b);
+       System.out.println(a<b);//Relational operator
+       System.out.println(a<=b);
+       System.out.println(a>b);
+       System.out.println(a>=b);
+       System.out.println(a==b);
+       System.out.println(a!=b);
+       
+
+	}
+
+}
